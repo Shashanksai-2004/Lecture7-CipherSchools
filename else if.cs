1d3cff0e33b83fcddf0e47125b16d2ade710b8e6
Shashanksai-2004 <103549184+Shashanksai-2004@users.noreply.github.com>
2023-07-06 10:29:00 +0530

    else if.cs

--- else if.cs
+++ else if.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public in score;
+    public int score;
     // Start is called before the first frame update
     void Start()
     {
